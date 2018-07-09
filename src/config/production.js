af480087b71605577fd35aff74bb2c80ed5cc9eb
Somprasong Damyos <somprasong@hospital-os.com>
2018-07-09 16:40:13 +0700

    use env prefix with app name

--- src/config/production.js
+++ src/config/production.js
@@ -6,9 +6,9 @@ dotenv.config();
 const whitelist = ['http://example1.com', 'http://example2.com'];
 
 export default {
-  PORT: process.env.PORT || 3000,
-  DB_URI: process.env.DB_URI,
-  JWT_SECRET: process.env.JWT_SECRET,
+  PORT: process.env.appName_PORT || 3000,
+  DB_URI: process.env.appName_DB_URI,
+  JWT_SECRET: process.env.appName_JWT_SECRET,
   CORS_OPTIONS: {
     origin(origin, callback) {
       if (whitelist.indexOf(origin) !== -1) {
