b520e0829c0b602d62103aeb58ed1d753f73d113
Somprasong Damyos <somprasong@hospital-os.com>
2018-07-13 15:29:40 +0700

    update middlewares/paginate.js to allow unknow query parameter

--- src/api/middlewares/paginate.js
+++ src/api/middlewares/paginate.js
@@ -2,16 +2,18 @@ import Joi from 'joi';
 import config from '../../config';
 
 export default (req, res, next) => {
-  const { error } = validate(req.query);
+  const { error, value } = validate(req.query);
   if (error) return res.status(400).json({ error: { message: error.details[0].message } });
 
-  req.query.limit = req.query.hasOwnProperty('limit') ? +req.query.limit : config.PAGE_LIMIT;
+  const { limit, offset, page } = value;
+
+  req.query.limit = req.query.hasOwnProperty('limit') ? limit : config.PAGE_LIMIT;
   if (req.query.hasOwnProperty('offset')) {
-    req.query.offset = +req.query.offset;
+    req.query.offset = offset;
     req.query.page = undefined;
   } else if (req.query.hasOwnProperty('page')) {
-    req.query.page = +req.query.page;
-    req.query.offset = (req.query.page - 1) * req.query.limit;
+    req.query.page = page;
+    req.query.offset = (page - 1) * limit;
   } else {
     req.query.offset = 0;
     req.query.page = 1;
@@ -21,16 +23,10 @@ export default (req, res, next) => {
 
 function validate(req) {
   const schema = {
-    limit: Joi.number()
-      .min(1)
-      .optional(),
-    offset: Joi.number()
-      .min(0)
-      .optional(),
-    page: Joi.number()
-      .min(1)
-      .optional(),
+    limit: Joi.number().min(1),
+    offset: Joi.number().min(0),
+    page: Joi.number().min(1),
   };
 
-  return Joi.validate(req, schema);
+  return Joi.validate(req, schema, { allowUnknown: true });
 }
