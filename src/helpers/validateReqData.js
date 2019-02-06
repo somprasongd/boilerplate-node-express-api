1ec2dee426f68a948dfff90af5f234e18b4e21ab
Somprasong Damyos <somprasong@hospital-os.com>
2019-02-06 17:22:43 +0700

    fix bug: joi allowUnkown

--- src/helpers/validateReqData.js
+++ src/helpers/validateReqData.js
@@ -1,6 +1,6 @@
 import Joi from 'joi';
 
-export default (data, schema, options = { allowUnknown: false, stripUnknown: true }) => {
+export default (data, schema, options = { allowUnknown: true, stripUnknown: true }) => {
   const { error, value } = Joi.validate(data, schema, options);
   if (error) {
     const err = new Error(error.details[0].message);
